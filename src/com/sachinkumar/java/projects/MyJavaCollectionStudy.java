aee30bdbce1f85d131c756eef7d14e8431e4dce8
Sachinkumar Gaikwad <gaikwadsachinkumar@gmail.com>
2023-01-21 10:43:20 +0530

    Push demo with access token

--- src/com/sachinkumar/java/projects/MyJavaCollectionStudy.java
+++ src/com/sachinkumar/java/projects/MyJavaCollectionStudy.java
@@ -11,7 +11,7 @@ public class MyJavaCollectionStudy {
 		this.animals=animals;
 	}
 	
-	//Constructor of the class
+	//Constructor of the class push demo
 	private void printAnimals(List<String> animals) {
 		for(String animal : animals) {
 			System.out.println(animal);
